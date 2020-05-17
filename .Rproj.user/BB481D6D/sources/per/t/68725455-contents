cf74407137c36925c0c568315508d2c63f30a243
Prima Wirawan <primailkom@gmail.com>
2020-05-18 03:34:53 +0700

    Initial commit

---
+++ .Rproj.user/BB481D6D/sources/per/t/68725455-contents
@@ -0,0 +1,9 @@
+library(plumber)
+library(linkgen)
+
+#* Return generated Mediafire link for download
+#* @param link Shared Mediafire link
+#* @post /linkgen
+function(link) {
+    mediafire_gen(link)
+}
---
+++ R/mediafire_gen.R
@@ -0,0 +1,22 @@
+#' Mediafire Link Generator
+#'
+#' @importFrom xml2 read_html
+#' @importFrom rvest html_nodes html_attr
+#' @importFrom magrittr %>%
+#'
+#' @param link Shared Mediafire link
+#'
+#' @return Generated Mediafire link for download
+#' @export
+#'
+#' @examples
+#' mediafire_gen("http://www.imdb.com/title/tt1490017/")
+mediafire_gen <- function(link) {
+  mediafire <- read_html(link)
+
+  link <- mediafire %>%
+    html_nodes(".download_link .input") %>%
+    html_attr("href")
+
+  return(link)
+}
