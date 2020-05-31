748cc1144fc4a32d94af64304f2962c09e2057d0
Prima Wirawan <primailkom@gmail.com>
2020-05-31 22:29:27 +0700

    Remove bundled API inside project

--- api/plumber.R
+++
@@ -1,9 +0,0 @@
-library(plumber)
-library(linkgen)
-
-#* Return generated Mediafire link for download
-#* @param link Shared Mediafire link
-#* @post /linkgen
-function(link) {
-    mediafire_gen(link)
-}
